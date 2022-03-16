bbf235f7fa516e337ec7a1fdb7f5fd1e0bc93308
WaterMaster-911 <330714638@qq.com>
2022-03-16 10:03:36 +0800

    Update parent_child_forest.h

--- 森林的双亲孩子表示法的设计与实现/parent_child_forest.h
+++ 森林的双亲孩子表示法的设计与实现/parent_child_forest.h
@@ -1,4 +1,22 @@
 #pragma once
 #include"Node.h"
+#include"Assistance.h"
 template<class ElemType>
 class ParentChildForest
+{
+protected:
+	// 森林的数据成员
+	ChildParentTreeNode<ElemType>* nodes;			// 存储树结点
+	int maxSize;									// 树结点最大个数 
+	int root, num;									// 根的位置及结点数
+
+	//	辅助函数:
+	void PreRootOrderHelp(int r, void (*Visit)(const ElemType&)) const;	// 先根序遍历
+	void PostRootOrderHelp(int r, void (*Visit)(const ElemType&)) const;	// 后根序遍历
+	int HeightHelp(int r) const;					// 返回以r为根的高
+	int DegreeHelp(int r) const;					// 返回以r为根的树的度
+
+public:
+	ParentChildForest();							//无参构造函数
+	virtual ~ChildParentTree();						// 析构函数
+};
