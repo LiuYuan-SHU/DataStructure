8615a343904f3eb8e97ac4d6ad037f4133399110
WaterMaster-911 <330714638@qq.com>
2022-03-16 10:31:04 +0800

    10：31

--- 森林的双亲孩子表示法的设计与实现/parent_child_forest.h
+++ 森林的双亲孩子表示法的设计与实现/parent_child_forest.h
@@ -22,6 +22,10 @@ public:
 	bool Empty() const;								// 判断森林是否为空
 	Status GetElem(int cur, ElemType& e) const;		// 用e返回结点元素值
 	Status SetElem(int cur, const ElemType& e);		// 将结cur的值置为e
+	int NodeCount() const;
+	int FirstChild(int cur) const;					// 返回结点cur的第一个孩子
+	int RightSibling(int cur) const;				// 返回结点cur的右兄弟
+	int Parent(int cur) const;
 };
 
 template<class ElemType>
@@ -78,4 +82,81 @@ Status ParentChildForest<ElemType>::SetElem(int cur, const ElemType& e)
 		nodes[cur].data = e;		// 将结点cur的值设置为e
 		return SUCCESS;	  		    // 返回SUCCESS
 	}
+}
+
+template <class ElemType>
+int ParentChildForest<ElemType>::NodeCount() const
+// 操作结果：返回森林的结点个数
+{
+	return num;
+}
+
+template <class ElemType>
+int ParentChildForest<ElemType>::FirstChild(int cur) const
+// 操作结果：如cur无孩子,则返回-1,否则返回树结点cur的第一个孩子,
+{
+	Node<int>* p;
+	if (cur < 0 || cur >= num)
+		return -1;						// 结点cur不存在,返回-1表示无孩子
+
+	if (nodes[cur].childLkList == NULL)	// 无孩子
+		return -1;
+	else
+		return nodes[cur].childLkList->data;	// 取出第一个孩子
+}
+
+template <class ElemType>
+int ParentChildForest<ElemType>::RightSibling(int cur) const
+// 操作结果：如果结点cur无右兄弟,则返回-1,否则返回cur的右兄弟
+{
+	if (cur < 0 || cur >= num)
+		return -1;						// 结点cur不存在,返回-1表示无孩子
+
+	int pt = nodes[cur].parent;			// cur的双亲
+	Node<int>* p = nodes[pt].childLkList;
+	while (p != NULL && p->data != cur)
+		p = p->next;
+	if (p == NULL || p->next == NULL)
+		return -1;				// 反回右兄弟
+	else
+		return p->next->data;	// 表示无右兄弟
+}
+
+template <class ElemType>
+int ParentChildForest<ElemType>::Parent(int cur) const
+// 操作结果：返回树结点cur的双亲
+{
+	if (cur < 0 || cur >= num)
+		return -1;						// 结点cur不存在,返回-1表示无双亲
+	return nodes[cur].parent;
+}
+
+
+template <class ElemType>
+ParentChildForest<ElemType>::ParentChildForest(ElemType items[], int parents[], int r, int n, int size)
+// 操作结果：建立数据元素为items[],对应结点双亲为parents[],根结点位置为r,结点个数为n的树
+{
+	maxSize = size;													// 最大结点个数
+	if (n > maxSize)
+		throw Error("结点个数太多!");								// 抛出异常
+	nodes = new ChildParentTreeNode<ElemType>[maxSize];				// 分配存储空间
+	for (int pos = 0; pos < n; pos++)
+	{	// 依次对结点数据元素及双亲位置进行赋值
+		nodes[pos].data = items[pos];								// 数据元素值
+		nodes[pos].parent = parents[pos];							// 双亲位置
+		if (parents[pos] != -1) {
+			Node<int>* p, * newnode;
+			newnode = new Node<int>(pos, NULL);
+			if (nodes[parents[pos]].childLkList == NULL)
+				nodes[parents[pos]].childLkList = newnode;
+			else {
+				p = nodes[parents[pos]].childLkList;
+				while (p->next != NULL)
+					p = p->next;
+				p->next = newnode;
+			}
+		}
+	}
+	root = r;														// 根
+	num = n;														// 结点个数
 }
