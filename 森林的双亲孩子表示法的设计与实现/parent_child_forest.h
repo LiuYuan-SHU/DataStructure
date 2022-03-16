be5332b3da45a41adbab4f8e49208d32c8eaf706
WaterMaster-911 <330714638@qq.com>
2022-03-16 10:13:51 +0800

    10：13

--- 森林的双亲孩子表示法的设计与实现/parent_child_forest.h
+++ 森林的双亲孩子表示法的设计与实现/parent_child_forest.h
@@ -6,9 +6,9 @@ class ParentChildForest
 {
 protected:
 	// 森林的数据成员
-	ChildParentTreeNode<ElemType>* nodes;			// 存储树结点
+	ChildParentTreeNode<ElemType>* nodes;			// 存储森林结点
 	int maxSize;									// 树结点最大个数 
-	int root, num;									// 根的位置及结点数
+	int *root, num;									// 根的位置及结点数
 
 	//	辅助函数:
 	void PreRootOrderHelp(int r, void (*Visit)(const ElemType&)) const;	// 先根序遍历
@@ -18,5 +18,38 @@ protected:
 
 public:
 	ParentChildForest();							//无参构造函数
-	virtual ~ChildParentTree();						// 析构函数
+	virtual ~ParentChildForest();						// 析构函数
+	bool Empty() const;								//判断森林是否为空
 };
+
+template<class ElemType>
+ParentChildForest<ElemType>::ParentChildForest()
+{
+	maxSize = DEFAULT_SIZE;								// 树结点最大个数
+	nodes = new ChildParentTreeNode<ElemType>[maxSize];	// 分配存储空间
+	root = -1;											// 表示不存在根
+	num = 0;											// 空森的结点个数为0
+}
+
+template <class ElemType>
+ParentChildForest<ElemType>::~ParentChildForest()
+// 操作结果：释放存储空间
+{
+	Node<int>* p;
+	for (int n = 0; n < num; n++) {
+		p = nodes[n].childLkList;
+		while (p != NULL) {
+			nodes[n].childLkList = p->next;
+			delete p;
+			p = nodes[n].childLkList;
+		}
+	}
+	delete[]nodes;										// 释放存储空间
+}
+
+template <class ElemType>
+bool ParentChildForest<ElemType>::Empty() const
+// 操作结果：判断森林是否为空
+{
+	return num == 0;
+}
