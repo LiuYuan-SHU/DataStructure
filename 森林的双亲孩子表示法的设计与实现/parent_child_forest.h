379a6b7a321056ca40b76b5dda31b1ef7510de33
WaterMaster-911 <330714638@qq.com>
2022-03-16 10:19:06 +0800

    Update parent_child_forest.h

--- 森林的双亲孩子表示法的设计与实现/parent_child_forest.h
+++ 森林的双亲孩子表示法的设计与实现/parent_child_forest.h
@@ -11,15 +11,17 @@ protected:
 	int *root, num;									// 根的位置及结点数
 
 	//	辅助函数:
-	void PreRootOrderHelp(int r, void (*Visit)(const ElemType&)) const;	// 先根序遍历
-	void PostRootOrderHelp(int r, void (*Visit)(const ElemType&)) const;	// 后根序遍历
+	void PreRootOrderHelp(int r, void (*Visit)(const ElemType&)) const;	  // 先根序遍历
+	void PostRootOrderHelp(int r, void (*Visit)(const ElemType&)) const;  //后根序遍历
 	int HeightHelp(int r) const;					// 返回以r为根的高
 	int DegreeHelp(int r) const;					// 返回以r为根的树的度
 
 public:
-	ParentChildForest();							//无参构造函数
-	virtual ~ParentChildForest();						// 析构函数
-	bool Empty() const;								//判断森林是否为空
+	ParentChildForest();							// 无参构造函数
+	virtual ~ParentChildForest();					// 析构函数
+	bool Empty() const;								// 判断森林是否为空
+	Status GetElem(int cur, ElemType& e) const;		// 用e返回结点元素值
+	Status SetElem(int cur, const ElemType& e);		// 将结cur的值置为e
 };
 
 template<class ElemType>
@@ -52,4 +54,28 @@ bool ParentChildForest<ElemType>::Empty() const
 // 操作结果：判断森林是否为空
 {
 	return num == 0;
+}
+
+template <class ElemType>
+Status ParentChildForest<ElemType>::GetElem(int cur, ElemType& e) const
+// 操作结果：用e返回结点cur元素值,如果不存在结点cur,函数返回NOT_PRESENT,否则返回ENTRY_FOUND
+{
+	if (cur < 0 || cur >= num)		// 不存在结点cur
+		return NOT_PRESENT;			// 返回NOT_PRESENT
+	else {	                        // 存在结点cur
+		e = nodes[cur].data;		// 用e返回元素值
+		return ENTRY_FOUND;			// 返回ENTRY_FOUND
+	}
+}
+
+template <class ElemType>
+Status ParentChildForest<ElemType>::SetElem(int cur, const ElemType& e)
+// 操作结果：如果不存在结点cur,则返回FAIL,否则返回SUCCESS,并将结点cur的值设置为e
+{
+	if (cur < 0 || cur >= num)		// 不存在结点cur
+		return FAIL;				// 返回FAIL
+	else {	                    // 存在结点cur
+		nodes[cur].data = e;		// 将结点cur的值设置为e
+		return SUCCESS;	  		    // 返回SUCCESS
+	}
 }
